6ba656f79e46073c6f2816dea49c5c77863bedd2
Jessie Li <236084590+jessie-qs-li@users.noreply.github.com>
2026-07-31 23:26:42 -0400

    Move Recent watches back to end of Beyond work section

--- app/page.tsx
+++ app/page.tsx
@@ -166,10 +166,6 @@ export default function Home() {
                 </div>
                 <a className="row-right" href="https://letterboxd.com/qishan_li/" target="_blank" rel="noopener">Letterboxd ↗</a>
               </div>
-              <div className="letterboxd-block">
-                <div className="letterboxd-recent-label">Recent watches</div>
-                <LetterboxdRecent />
-              </div>
               <div className="cv-row">
                 <svg className="row-icon" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" aria-hidden><path d="M12 20h9"/><path d="M16.5 3.5a2.121 2.121 0 0 1 3 3L7 19l-4 1 1-4L16.5 3.5z"/></svg>
                 <div className="row-body">
@@ -202,6 +198,10 @@ export default function Home() {
                 </div>
                 <a className="row-right" href="https://www.instagram.com/p/DAwwBkyPLXY/" target="_blank" rel="noopener">Photos ↗</a>
               </div>
+            </div>
+            <div className="letterboxd-block">
+              <div className="letterboxd-recent-label">Recent watches</div>
+              <LetterboxdRecent />
             </div>
           </section>
 
